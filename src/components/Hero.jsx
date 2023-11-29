13b1585b41ff81db216c223cc8ea9829e83a06ce
RazaNaqsh <owaisraza1704@gmail.com>
2023-11-29 22:54:45 +0530

    navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import hero from "../assets/hero.jpg";
+const Hero = () => {
+  return (
+    <div className="pt-12 relative">
+      <img
+        src={hero}
+        className="z-0 opacity-75 ml-auto mr-[15vw] h-[75vh]"
+        alt=""
+      />
+      <div className="absolute top-28 left-60 z-20 tracking-widest">
+        <h1 className="text-[80px] font-bold ml-36 leading-[70px]">
+          Next Level
+        </h1>
+        <h1 className="text-[80px] font-bold leading-[100px]">Amazing</h1>
+        <h1 className="text-[80px] font-bold ml-36 leading-[100px]">
+          Videograper
+        </h1>
+
+        <button className="ml-36 text-[18px] px-5 py-4 bg-[#FFA33E] rounded-[100px] mt-8">
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-[#010207] text-white">
+      <div className="flex justify-between w-[70%] mx-auto items-center py-[20px]">
+        <img src={logo} className="w-[120px]" alt="" />
+        <ul className="flex gap-20">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Pages</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
