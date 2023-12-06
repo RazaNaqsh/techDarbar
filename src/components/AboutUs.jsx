485aec346532874d8d20a184821fbe97dd434c3e
RazaNaqsh <owaisraza1704@gmail.com>
2023-12-06 12:38:34 +0530

    About us section responsive

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,15 +1,15 @@
 import about from "../assets/about.jpg";
 const AboutUs = () => {
   return (
-    <div className="py-20 flex items-center w-[85%] mx-auto gap-20">
-      <img src={about} className="w-[40vw]" alt="" />
+    <div className="py-20 flex flex-col md:flex-row items-center w-[85%] mx-auto gap-20">
+      <img src={about} className="w-[80vw] md:w-[40vw]" alt="" />
       <div>
         <h3>About Us</h3>
         <h2 className="text-[50px] font-bold leading-[60px]">
           We Always Make <br /> The Best
         </h2>
         <hr className="my-4 w-[100px] border-2 border-red-500" />
-        <p className=" text-[17px] w-[35vw] mx-auto text-[#8F8F8F]">
+        <p className=" text-[17px] w-[80vw] md:w-[35vw] mx-auto text-[#8F8F8F]">
           We are a creative film and photo production company hungry for quality
           in aesthetics. To create modern recognizable stuff we are working with
           a strong network of experienced professionals. We set up teams to
