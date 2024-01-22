18d3a0f8916d2636a8c2c4d0e6c3124e0400df75
RazaNaqsh <owaisraza1704@gmail.com>
2024-01-22 15:26:13 +0530

    Service section animations

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,23 +2,43 @@ import video from "../assets/video.svg";
 import camera from "../assets/camera.svg";
 import videography from "../assets/videography.svg";
 
+import { motion } from "framer-motion";
+
 const Services = () => {
   return (
     <section className="text-white py-20">
       <div className="space-y-2">
-        <h1 className="font-cinzel text-[30px] md:text-[44px] font-semibold text-center">
+        <motion.h1
+          className="font-cinzel text-[30px] md:text-[44px] font-semibold text-center"
+          initial={{ opacity: 0, y: -70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          viewport={{ once: true }}
+        >
           Filming Services
-        </h1>
+        </motion.h1>
         <hr className="mx-auto w-[100px] border-2 border-red-500" />
-        <p className=" text-justify md:text-center text-[14px] md:text-[17px] w-[80vw] md:w-[45vw] mx-auto text-[#8F8F8F]">
+        <motion.p
+          className=" text-justify md:text-center text-[14px] md:text-[17px] w-[80vw] md:w-[45vw] mx-auto text-[#8F8F8F]"
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          viewport={{ once: true }}
+        >
           I provide a range of creative services including video editing,
           wedding filming, and camerawork. From crafting compelling stories to
           capturing special moments, feel free to reach out with any inquiries
           or questions.
-        </p>
+        </motion.p>
       </div>
       <div className=" flex flex-col md:flex-row items-center gap-12 md:gap-0 justify-around w-[90%] mx-auto mt-10">
-        <div className="w-[85vw] md:w-[30%] flex flex-col items-center px-4 space-y-2">
+        <motion.div
+          className="w-[85vw] md:w-[30%] flex flex-col items-center px-4 space-y-2"
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+        >
           <img
             src={video}
             className="w-[120px] md:w-[150px] big:w-[180px] lg:w-[200px]"
@@ -31,8 +51,14 @@ const Services = () => {
             documentary, I have the skills to elevate your project to the next
             level.
           </p>
-        </div>
-        <div className="w-[85vw] md:w-[30%] flex flex-col items-center px-4 space-y-2">
+        </motion.div>
+        <motion.div
+          className="w-[85vw] md:w-[30%] flex flex-col items-center px-4 space-y-2"
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+        >
           <img
             src={camera}
             className="w-[120px] md:w-[150px] big:w-[180px] lg:w-[200px]"
@@ -44,8 +70,14 @@ const Services = () => {
             for diverse projects, from commercials to short films. Elevate your
             storytelling with my creative lens and expertise in videography
           </p>
-        </div>
-        <div className="w-[85vw] md:w-[30%] flex flex-col items-center px-4 space-y-2">
+        </motion.div>
+        <motion.div
+          className="w-[85vw] md:w-[30%] flex flex-col items-center px-4 space-y-2"
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+        >
           <img
             src={videography}
             className=" w-[120px] md:w-[150px] big:w-[180px] lg:w-[200px]"
@@ -57,7 +89,7 @@ const Services = () => {
             eye for emotion and aesthetics, I capture the essence of your
             special day, creating a timeless and beautiful wedding film.
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
